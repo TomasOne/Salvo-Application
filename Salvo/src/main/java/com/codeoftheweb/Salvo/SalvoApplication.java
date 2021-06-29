3931e11f741fd4d2f91198dd8bc898f605133b9f
Tomas.One <onetto_tomas@hotmail.com>
2021-06-29 14:54:05 -0300

    Primer día
    Creación de la clase Player, su repositorio y la primer implementación con Front-End. Ya se puede agragar ( de forma memórica) nuevos players a la DataBase registrando según sus correos

--- Salvo/src/main/java/com/codeoftheweb/Salvo/SalvoApplication.java
+++ Salvo/src/main/java/com/codeoftheweb/Salvo/SalvoApplication.java
@@ -1,7 +1,9 @@
 package com.codeoftheweb.Salvo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SalvoApplication {
@@ -10,4 +12,12 @@ public class SalvoApplication {
 		SpringApplication.run(SalvoApplication.class, args);
 	}
 
+	@Bean//Palabra reservada para JavaBean. Se utiliza para suministrar datos a una clase sin que la clase tenga que crearlos
+	public CommandLineRunner initData(PlayerRepocitory repository) {
+		return (args) -> {
+			// Agrega datos a nuestro repocitory y se envían a la DB
+			repository.save(new Player("testmail@test.com"));
+		};
+	}
 }
+
